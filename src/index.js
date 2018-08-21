930542524247f2953326850d7e8cde072038ba2a
Roman Makarov <7980900@gmail.com>
2018-08-21 13:28:19 +0300

    adds linter to project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export const sayHello = () => {
+const sayHello = () => {
   console.log('Welcome to the Brain Games!\n');
   const askforName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${askforName}`);
   return askforName;
 };
+
+export default sayHello;
