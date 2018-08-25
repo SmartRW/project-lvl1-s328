4b64f71e15c4f4dd904da4a9cc19485ef988b3f4
SmartRW <39382405+SmartRW@users.noreply.github.com>
2018-08-25 11:41:56 +0300

    adds progression game

---
+++ src/games/progression.js
@@ -0,0 +1,33 @@
+import playGame from '..';
+import generateNumber from '../utils';
+import { cons } from 'hexlet-pairs';
+
+const gameRule = 'What number is missing in this progression?';
+
+const startItemMin = 1;
+const startItemMax = 9;
+const startItem = generateNumber(startItemMin, startItemMax);
+const progressionLength = 10;
+const hiddenItemIndex = generateNumber(0, progressionLength - 1);
+const minStep = 1;
+const maxStep = 9;
+const progressionStep = generateNumber(minStep, maxStep);
+
+const generateCondition = () => {
+  let question = '';
+  const hiddenItem = startItem + (hiddenItemIndex * progressionStep);
+  const correctAnswer = String(hiddenItem);
+  for (let i = 0; i < progressionLength; i += 1) {
+    if (i === hiddenItemIndex) {
+      question += '.. ';
+    } else {
+      const nthItem = startItem + (i * progressionStep);
+      question += `${nthItem} `;
+    }
+  }
+  return cons(question, correctAnswer);
+};
+
+const playProgression = () => playGame(gameRule, generateCondition);
+
+export default playProgression;
