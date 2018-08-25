7125f8ee746b440c6186de358d3e70ebf04471dc
SmartRW <39382405+SmartRW@users.noreply.github.com>
2018-08-25 21:06:57 +0300

    fix due mentor's review

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,25 +4,28 @@ import { cons } from 'hexlet-pairs';
 
 const gameRule = 'Is this number prime?';
 
-const isPrime = (num, devisor) => {
-  if (devisor > num / 2) {
-    return true;
-  }
-  if (num % devisor === 0) {
-    return false;
-  }
+const isPrime = (num) => {
+  const iter = (number, devisor) => {
+    if (devisor > num / 2) {
+      return true;
+    }
+    if (num % devisor === 0) {
+      return false;
+    }
 
-  return isPrime(num, devisor + 1);
+    return isPrime(num, devisor + 1);
+  };
+  const minDevisor = 2;
+  return iter(num, minDevisor);
 };
 
 const minNumber = 1;
 const maxNumber = 99;
 
-const minDevisor = 2;
 
 const generateCondition = () => {
   const number = generateNumber(minNumber, maxNumber);
-  const correctAnswer = isPrime(number, minDevisor) ? 'yes' : 'no';
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
   const question = number;
   return cons(question, correctAnswer);
 };
